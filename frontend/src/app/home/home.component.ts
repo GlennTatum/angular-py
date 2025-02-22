83804bdeab021d5dd0ea9ec4bcb71c2b1f14d291
GlennTatum <glennetatum@gmail.com>
2025-02-21 23:24:56 -0500

    mathlive integration

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { MathfieldElement, Mathfield } from 'mathlive';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,20 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
+
+  ngAfterViewInit(): void {
+    let mfe = new MathfieldElement();
+
+    // Set initial value and options
+    mfe.value = "\\frac{\\sin(x)}{\\cos(x)}";
+
+    // Options can be set either as an attribute (for simple options)...
+    mfe.setAttribute('virtual-keyboard-layout', 'dvorak');
+
+    // Attach the element to the DOM
+    let mathLiveDiv = document.getElementById("mathLive");
+    mathLiveDiv.appendChild(mfe);
+  }
 
 }
